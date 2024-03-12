aaeb2b78dcb175d0316f13378238c5b338690b2c
zahidjaved <zahidjaved121@gmail.com>
2024-03-12 12:36:13 -0700

    Coop Crowd

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// SWIPER SLIDER JS
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
